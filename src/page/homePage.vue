e5ecc7f03698892a47b2f039e3166217e3c1bafe
ying <ying@yingdeMacBook-Pro.local>
2018-01-19 20:10:42 +0800

    edit

--- src/page/homePage.vue
+++ src/page/homePage.vue
@@ -2,6 +2,7 @@
   <div class="homePage">
     <div class="itemOne"><span>简历</span><span>Resume</span></div>
     <div class="itemTwo"><span>{{ homePage.zhName}}</span><span>{{ homePage.enName }}</span></div>
+    <div class="itemThree">{{ homePage.level }}</div>
   </div>
 </template>
 
@@ -23,8 +24,8 @@ export default {
       // this.$axios.get('/api/posts', {}).then(res => {
       this.$axios.get('https://easy-mock.com/mock/5a01524d36a23b429ea925c5/example/homePage').then(res => {
         console.log(res)
-        if (res.success) {
-          this.homePage = res.data
+        if (res.data.success) {
+          this.homePage = res.data.data
         }
       }).catch(err => {
         console.log(err)
@@ -43,6 +44,46 @@ export default {
     background-image: url('../assets/image/bg.jpg');
     width: 100%;
     height: 100%;
+    color: #fff;
+    > .itemOne {
+      padding-top: 180px;
+      font-size: 60px;
+      span:nth-child(1) {
+        letter-spacing: 5px;
+      }
+      span:nth-child(2) {
+        margin-left: 20px;
+      }
+    }
+    > .itemTwo {
+      margin-top: 40px;
+      padding-top: 40px;
+      font-size: 28px;
+      position: relative;
+      span:nth-child(1) {
+        letter-spacing: 5px;
+      }
+      span:nth-child(2) {
+        margin-left: 20px;
+      }
+    }
+    > .itemTwo:before {
+      content: "";
+      display: inline-block;
+      position: absolute;
+      left: 45%;
+      right: 45%;
+      top: 0;
+      width: 10%;
+      height: 1px;
+      margin: auto;
+      background-color: #fff;
+    }
+    > .itemThree {
+      font-size: 20px;
+      padding-top: 10px;
+      letter-spacing: 1px;
+    }
     > div {
       @include rounded-corners;
     }
